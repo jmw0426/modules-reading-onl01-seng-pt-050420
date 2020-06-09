89cae62a14023e0d5f9cee38831a9e509f9062c7
Joshua Williams <jmw0426@hotmail.com>
2020-06-09 17:22:51 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,13 +2,12 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance 
-  
-  attr_accessor :name 
-  
+  include Dance
+ 
+  attr_accessor :name
+ 
   def initialize(name)
-    @name = name 
-  end 
-  
-end 
+    @name = name
+  end
+end
 
