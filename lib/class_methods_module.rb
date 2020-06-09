a39548af04ec0b3df32a25e6a9baa174d816b15d
Joshua Williams <jmw0426@hotmail.com>
2020-06-09 17:23:42 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -6,10 +6,10 @@ module MetaDancing
   
 end
 
-class Dancer
-  extend MetaDancing
-end
+# class Dancer
+#   extend MetaDancing
+# end
 
-class Kid
-  extend MetaDancing
-end
+# class Kid
+#   extend MetaDancing
+# end
