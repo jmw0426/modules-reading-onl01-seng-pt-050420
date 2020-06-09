1c6657b111358b4b7a6468829768f88d61ecaaeb
Joshua Williams <jmw0426@hotmail.com>
2020-06-09 17:22:00 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,6 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  
   include Dance 
   
   attr_accessor :name 
@@ -13,6 +12,3 @@ class Kid
   
 end
 
-class Kid
-  extend MetaDancing
-end
